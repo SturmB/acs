07cf469406dea6c54074ae17af2ebc1bedcdc4d4
Chris McGee <chris@chrismcgee.info>
2018-03-07 14:44:16 -0500

    # Sidebar Complete #
    * Completes work on the sidebar, including styling.
    * Adds FontAwesome 5 Pro.
    * Adds more Sass variables, including colors, a gradient, a font, and a standard size for borders.
    * Adjusts the styling for all of the columns to make sure there is spacing between columns.
    * Other minor styling changes.
    
    Signed-off-by: Chris McGee <chris@chrismcgee.info>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
         @include('layouts.layout_main')
 
 
-        <script type="text/javascript" src={{ asset('js/app.js') }}></script>
+        <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('vendor/fontawesome-pro-5.0.6/svg-with-js/js/fontawesome-all.min.js') }}"></script>
 
     </body>
 </html>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,37 +1,46 @@
 <div class="row">
-    <nav class="col navbar navbar-expand-lg navbar-light bg-gradient-light rounded">
+    <div class="col">
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <nav class="navbar navbar-expand-lg navbar-light bg-gradient-light rounded">
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            <ul class="navbar-nav nav-fill w-100">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                <!-- Products Menu -->
-                {!! $productsHtml !!}
+                <ul class="navbar-nav nav-fill w-100">
 
-                <!-- Clipart Menu -->
-                {!! $clipartHtml !!}
+                    <!-- Products Menu -->
+                    {!! $productsHtml !!}
 
-                <!-- Information Menu -->
-                <li class='nav-item dropdown'>
-                    <a class='nav-link dropdown-toggle' href='#' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                        Information
-                    </a>
-                    <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
-                        <a class='dropdown-item' href="orderform.pdf" target="_blank" title="Order Form">Order Form</a>
-                        <a class='dropdown-item' href="typefaces.php" target="_self" title="Fonts">Typefaces</a>
-                        <a class='dropdown-item' href="general_information.php" target="_self" title="General Information">General Info</a>
-                        <a class='dropdown-item' href="about.php" target="_self" title="About Us">About Us</a>
-                        <a class='dropdown-item' href="contact.php" target="_self" title="Contact Information">Contact Info</a>
-                    </div>
-                </li>
+                    <!-- Clipart Menu -->
+                    {!! $clipartHtml !!}
 
-            </ul>
+                    <!-- Information Menu -->
+                    <li class='nav-item dropdown'>
+                        <a class='nav-link dropdown-toggle' href='#' role='button' data-toggle='dropdown'
+                           aria-haspopup='true' aria-expanded='false'>
+                            Information
+                        </a>
+                        <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                            <a class='dropdown-item' href="orderform.pdf" target="_blank" title="Order Form">Order
+                                Form</a>
+                            <a class='dropdown-item' href="typefaces.php" target="_self" title="Fonts">Typefaces</a>
+                            <a class='dropdown-item' href="general_information.php" target="_self"
+                               title="General Information">General Info</a>
+                            <a class='dropdown-item' href="about.php" target="_self" title="About Us">About Us</a>
+                            <a class='dropdown-item' href="contact.php" target="_self" title="Contact Information">Contact
+                                Info</a>
+                        </div>
+                    </li>
 
-        </div>
+                </ul>
 
-    </nav>
-</div>
+            </div>
+
+        </nav>
+
+    </div> {{--col--}}
+</div> {{--row--}}
