737d0d995ed8008cb186d083fadbb4f1ac3d9d75
Chris McGee <chris@chrismcgee.info>
2018-07-13 14:50:06 -0400

    # FontAwesome 5.1.0 #
    * Updates `FontAwesome` to v5.1.0.
    * Fixes a bad entry in `.gitignore`.
    
    Signed-off-by: Chris McGee <chris@chrismcgee.info>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 
         <!-- FontAwesome 5 Pro -->
-        <script type="text/javascript" src="{{ asset('vendor/fontawesome-pro-5.0.6/svg-with-js/js/fontawesome-all.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('vendor/fontawesome-pro-5.1.0-web/js/all.js') }}"></script>
 
         <!-- FlexiNav -->
         <script type="text/javascript" src="{{ asset('vendor/flexinav/js/jquery.js') }}"></script>
@@ -19,16 +19,6 @@
         <script type="text/javascript" src="{{ asset('vendor/flexinav/js/flexinav.min.js') }}"></script>
 
         <!-- Miscellaneous scripts -->
-{{-- Moved to `navbar.blade.php`.
-        <script>
-            $(function () {
-                // Add the 'last' class to the last Subcategory of each Menu Category.
-                // This adds a bit of space to the bottom of the Menu Category's flyout.
-                $('.dropdown_flyout').children('li:last-child').addClass('last');
-            });
-        </script>
---}}
-
         @stack('scripts')
 
     </body>
