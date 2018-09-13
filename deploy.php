84b107725cdb1c2c60daa84ab63071f6f7e4c639
Chris McGee <chris@chrismcgee.info>
2018-09-13 11:16:32 -0400

    Privatizing
    * Updates `.gitignore` to allow paid vendor files to be committed and pushed.
    * Adds `deploy.php` for [deployer.org](http://deployer.org) deployment scripting.
    
    Signed-off-by: Chris McGee <chris@chrismcgee.info>

---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'American Cabin Supply');
+
+// Project repository
+set('repository', 'https://github.com/SturmB/acs.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('skyubuntu')
+    ->become('sturm')
+    ->set('deploy_path', '/var/www/americancabin.com');
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
