3be78b5722bb77aaa1219911d794f4a05a3ab961
Chris McGee <chris@chrismcgee.info>
2021-02-04 11:54:00 -0500

    :zap: Adds caching to the `ClipartController`
    Signed-off-by: Chris McGee <chris@chrismcgee.info>

--- app/Http/Controllers/ClipartController.php
+++ app/Http/Controllers/ClipartController.php
@@ -3,27 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\ClipartSubcategory;
-use Illuminate\Http\Request;
+use Cache;
+use DateInterval;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\View\View;
 
 class ClipartController extends Controller
 {
-    /**
-     * Show the Clipart view for a given category.
-     *
-     * @param $subcategory
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function index($subcategory)
-    {
-        // Get the Model for the given subcategory id.
-        $clipartSubcategory = ClipartSubcategory::with('clipartCategory')->find(
-            $subcategory
-        );
+  /**
+   * Show the Clipart view for a given category.
+   *
+   * @param $subcategory
+   * @return Factory|View
+   */
+  public function index($subcategory)
+  {
+    // Get the Model for the given subcategory id.
+    $clipartSubcategory = Cache::remember(
+      'clipart:subcategory',
+      new DateInterval('P1M'),
+      function () use ($subcategory) {
+        return ClipartSubcategory::with('clipartCategory')->find($subcategory);
+      }
+    );
 
-        // Get all of the images in the clipart folder for the subcategory.
-        $allFiles = scandir(config('global.clipartPath') . $subcategory);
-        $images = preg_grep('/^[0-9A-Za-z].*\.(svg|png|jpg)$/', $allFiles);
+    // Get all of the images in the clipart folder for the subcategory.
+    $allFiles = scandir(config('global.clipartPath') . $subcategory);
+    $images = preg_grep('/^[0-9A-Za-z].*\.(svg|png|jpg)$/', $allFiles);
 
-        return view('clipart', compact('clipartSubcategory', 'images'));
-    }
+    return view('clipart', compact('clipartSubcategory', 'images'));
+  }
 }
