31a57f647c7d6c67a1a3d026f4a749f46184dfdc
Chris McGee <chris@chrismcgee.info>
2021-02-12 15:12:30 -0500

    :green_heart: Band-aid fix for the deletion problems during deployment
    Signed-off-by: Chris McGee <chris@chrismcgee.info>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,8 @@
 set -e
 ssh-keyscan -H "$IP" >>~/.ssh/known_hosts
 
-# Remove existing link if it exists
-ssh -t "$USER_NAME"@"$IP" "cd $DEPLOY_PATH && unlink $DEPLOY_PATH/public/storage 2> /dev/null"
-
 # Copy files to the server
-rsync -aO --chown=www-data:www-data --fake-super --exclude='.git' --exclude='.env' --del ./ "$USER_NAME"@"$IP":"$DEPLOY_PATH"
+rsync -aO --chown=www-data:www-data --fake-super --exclude='.git' --exclude='.env' ./ "$USER_NAME"@"$IP":"$DEPLOY_PATH"
 
 # Install dependencies
 ssh -t "$USER_NAME"@"$IP" "cd $DEPLOY_PATH && composer install --no-progress --no-interaction --optimize-autoloader --no-dev"
